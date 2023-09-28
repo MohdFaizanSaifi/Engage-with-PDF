c97cb09e6576ab6cca17a9455c9a1f0f21135558
ShahFaizans <saifishah0001@gmail.com>
2023-09-29 00:17:09 +0530

    version3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores  import FAISS
-from langchain.llms import OpenAIChat
+from langchain.llms import OpenAIChat, HuggingFaceHub
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
@@ -29,13 +29,15 @@ def get_text_chunks(text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    #embeddings = HuggingFaceInstructEmbeddings(model_name = "barisaydin/bge-large-en")
+    #embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name = "barisaydin/gte-small")
     vectorscore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorscore
 
 def get_conversation_chain(vectorstore):
-    llm = OpenAIChat(openai_api_key=OPENAI_API_KEY)
+    #llm = OpenAIChat()
+
+    llm = HuggingFaceHub(repo_id="tiiuae/falcon-7b", model_kwargs={"temperature":0.7, "max_length":512})
     memory = ConversationBufferMemory(memory_key = 'chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
